8190de62437d5c9f7f973a6c42a3a7fbdbd3dbe5
peter <peter@kinamo.be>
2022-09-05 11:56:35 +0200

    number required fix

--- src/components/AbstractModelGenerator.php
+++ src/components/AbstractModelGenerator.php
@@ -479,14 +479,14 @@ abstract class AbstractModelGenerator extends Component
                     $rules[] = "['$columnName', 'email']";
                 }
 
-                if ($columnSchema->defaultValue !== null) {
-                    if (is_string($columnSchema->defaultValue)) {
-                        $v = str_replace("'","\\'", $columnSchema->defaultValue);
-                        $rules[] = "['$columnName', 'default', 'value' => '{$v}']";
-                    } else {
-                        $rules[] = "['$columnName', 'default', 'value' => {$columnSchema->defaultValue}]";
-                    }
-                }
+//                if ($columnSchema->defaultValue !== null) {
+//                    if (is_string($columnSchema->defaultValue)) {
+//                        $v = str_replace("'","\\'", $columnSchema->defaultValue);
+//                        $rules[] = "['$columnName', 'default', 'value' => '{$v}']";
+//                    } else {
+//                        $rules[] = "['$columnName', 'default', 'value' => {$columnSchema->defaultValue}]";
+//                    }
+//                }
             }
 
             foreach ($uniqueIndexes as $indexName => $columns) {
